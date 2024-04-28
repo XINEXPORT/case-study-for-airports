48a9bf1fb0b8b0e924636c686e09c4575dae8290
Christine Hoang <christinehoangt@gmail.com>
2024-04-28 10:51:11 -0500

    column headers snaked case and lowercased

--- CaseStudyHoang.R
+++ CaseStudyHoang.R
@@ -9,9 +9,13 @@ tsa_claims1 <- read_csv("tsa_claims1.csv")
 tsa_claims1
 
 # What is the most common type of insurance claim?
+
 # Which claim site within the airport are claims most commonly filed for?
+
 # What type of claim is made most at each claim site? Hint: You can group by multiple columns.
+
 # What is the median claim amount?
+
 # What is the overall claim approval rate for the entire U.S.? Hint: You can get the number of claims for each status add then add a column that uses the sum() function to calculate the total number of claims.
 
 
